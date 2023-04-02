2bc56016e79dccb095d95174c9aa3eb6da6b9e6d
madhusudan-deshpande <madhusudandeshpande28@gmail.com>
2023-04-02 16:57:35 -0400

    a8 REST Spring 2023

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+let API_BASE = process.env.REACT_APP_API_BASE;
+API_BASE = "https://tuiter-node-server-app-r57b.onrender.com/api";
+// API_BASE = 'http://localhost:4000/api';
+const TUITS_API = `${API_BASE}/tuits`
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
